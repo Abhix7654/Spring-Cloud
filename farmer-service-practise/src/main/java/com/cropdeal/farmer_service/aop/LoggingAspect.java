2d969bd98e7f4432bacaf4935df53b6fcd959c5e
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-03-24 14:56:30 +0530

    instead of sout we use info with the help of logger

--- farmer-service-practise/src/main/java/com/cropdeal/farmer_service/aop/LoggingAspect.java
+++ farmer-service-practise/src/main/java/com/cropdeal/farmer_service/aop/LoggingAspect.java
@@ -2,13 +2,17 @@ package com.cropdeal.farmer_service.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class) ;
     @Before("execution(public * com.cropdeal.farmer_service.controller.FarmerController.getAllFarmers())")
     public void log(){
-        System.out.println("GetAllFarmers Called");
+        LOGGER.info("GetAllFarmers Called");
     }
 }
